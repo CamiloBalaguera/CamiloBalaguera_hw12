f4310fd903ca4d0456a02f042acc70ed69d09c62
Camilo Balaguera <ca.balaguera@uniandes.edu.co>
2018-04-24 16:55:20 -0500

    Se agregaron comentarios a todos los archivos

--- CB_advection.cpp
+++ CB_advection.cpp
@@ -1,26 +1,30 @@
 #include <iostream>
 #include <stdlib.h>
 using namespace std;
-const float ini = -2.0;
-const float fin = 2.0;
-const float c = 1.0;
-const int nt = 700;
-const float dt = 0.001;
-const float dx = 45*dt;
-const int nx = (fin - ini)/dx;
-float x[nx];
-float u[nx][nt];
-void evolucion(void);
+const float ini = -2.0; // Valor inicial
+const float fin = 2.0; // Valor final
+const float c = 1.0; // VElocidad
+const int nt = 700; // Numero de datos temporales
+const float dt = 0.001; // Diferencial de tiempo
+const float dx = 45*dt; // Diferencial de espacio
+const int nx = (fin - ini)/dx; // Numero de datos espaciales
+float x[nx]; // Lista de posiciones
+float u[nx][nt]; // Matriz de amplitudes. Primera dimension espacial. Segunda dimension temporal
+void evolucion(void); // Funcion que evoluciona
 
 int main()
 {
-	cout << nx << " " << nt << endl;
+	cout << nx << " " << nt << endl; // Se imprime el numero de datos espaciales y temporales para utilizar en el .py
+	// Condiciones de frontera	
 	for (int i = 0; i < nt ; i++)
 	{
 		u[0][i] = 0.0;
 		u[nx-1][i] = 0.0;
 	}
+	// Valor inicial de posicion
 	x[0] = -2.0;
+
+	//Condiciones iniciales.
 	for (int i = 1; i < nx; i ++)
 	{
 		x[i] = x[i-1] + dx;
@@ -33,24 +37,27 @@ int main()
 			u[i][0] = 0.0;
 		}
 	}
+	
+	// Evolucion de la amplitud
 	evolucion();
 	return 0;
 }
 
+// Funcion que evoluciona la funcion amplitud(x, t)
 void evolucion (void)
 {
 	for (int j = 1; j < nt; j++)
 	{
 		for(int i = 1; i < nx; i++)
 		{
-			u[i][j] = u[i][j-1] - c*(dt/dx)*(u[i][j-1]-u[i-1][j-1]);
+			u[i][j] = u[i][j-1] - c*(dt/dx)*(u[i][j-1]-u[i-1][j-1]); // Evolución.
 		}
 	}
 	for (int k = 0; k < nt; k+=nt/5)
 	{
 		for (int i = 0; i < nx; i++)
 		{
-			cout << x[i] << " " << u[i][k] << endl;
+			cout << x[i] << " " << u[i][k] << endl; // Se imprimen todas las posiciones para 5 tiempos particulares.
 		}
 	}
 
--- CB_graph.py
+++ CB_graph.py
@@ -1,15 +1,18 @@
 import numpy as np
 import matplotlib.pyplot as plt
+
 numero = np.genfromtxt("advection.txt", usecols = (0))
 pos = np.genfromtxt("advection.txt", usecols = (0))
 amp = np.genfromtxt("advection.txt", usecols = (1))
-n = int(numero[0])
-posicion = pos[1:n]
-amp1 = amp[1:n]
-amp2 = amp[n:2*n-1]
-amp3 = amp[2*n:3*n-1]
-amp4 = amp[3*n:4*n-1]
-amp5 = amp[4*n:5*n-1]
+
+n = int(numero[0]) # Numero de espacios correspondientes a todas las posiciones
+
+posicion = pos[1:n] # Lista de todas las posiciones
+amp1 = amp[1:n] # Lista de amplitudes para todas las posiciones en tiempo 0
+amp2 = amp[n:2*n-1] # Lista de amplitudes para todas las posiciones en tiempo 1
+amp3 = amp[2*n:3*n-1] # Lista de amplitudes para todas las posiciones en tiempo 2
+amp4 = amp[3*n:4*n-1] # Lista de amplitudes para todas las posiciones en tiempo 3
+amp5 = amp[4*n:5*n-1] # Lista de amplitudes para todas las posiciones en tiempo 4
 plt.plot(posicion,amp1, label = "Tiempo 0")
 plt.plot(posicion,amp2, label = "Tiempo 1")
 plt.plot(posicion,amp3, label = "Tiempo 2")
